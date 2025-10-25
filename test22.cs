f81905ddcde81d382cb12ab850ad2c24d890488f
Mathias Rentsch <rentsch@online.de>
2025-10-25 12:57:30 +0200

    Test25102025

--- test22.cs
+++ test22.cs
@@ -3,13 +3,7 @@ class Konzert
 {
     int Freddy;
     int Bryan;
-<<<<<<< HEAD
-    int Vicky;
-    int Phil;
-    int Frank;
-=======
     int Phil;
     int Frank;
     int Vicky;
->>>>>>> bc202bb742b58aaf3b5212b75255e578216c4daa
 }
